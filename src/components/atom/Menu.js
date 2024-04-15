9828ad7ca8ffd5d0a3ac1841f0f6d3ad09881396
sahil kumar <1903041037Sahil@gamil.com>
2024-04-15 19:06:11 +0530

    Header component completed

---
+++ src/components/atom/Menu.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Menu.css";
+const Menu = () => {
+  return (
+    <div className="Menu-container">
+      <div className="active">Women</div>
+      <p>Male</p>
+      <p>Mother-Child</p>
+      <p>Home & Furniture</p>
+      <p>Super market</p>
+      <p>Cosmetics</p>
+      <p>Shoe & Bag</p>
+      <p>Electonic</p>
+      <p>Sport & outdoor</p>
+      <p>Best seller</p>
+    </div>
+  );
+};
+export default Menu;
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+const homepage = () => {
+  return (
+    <div className="pages">
+      <div className="home">
+        Homepage <img src={"#"} /> About us
+      </div>
+
+      <div className="head">
+        <p>
+          Let's get to know Luminae Providing the best experience to make your
+          Online Shopping At Luminae, we are more than just an online store – we
+          are your ultimate destination for an unparalleled shopping experience.
+          Our journey began with a simple yet powerful idea: to create a
+          platform that not only offers a wide array of products but also
+          fosters a sense of community and connection among our customers. 20+
+          10+ 4M+ Shopping category Different Territory Happy Client
+        </p>
+      </div>
+    </div>
+  );
+};
+export default homepage;
